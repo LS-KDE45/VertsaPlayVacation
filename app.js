00e2ffe63dfd3a0b0e7339189e132f5cca3f30d8
LS-KDE45 <luisqueiros21@gmail.com>
2025-03-28 19:34:27 +0000

    old code cleanup in app

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use((req, res, next) => {
 
 //index
 app.get("/", (req, res) => res.render("index"));
-//other views
-//app.get("/login", (req, res) => res.render("login"));
-//app.get("/createUser", (req, res) => res.render("createUser"));
-/*
-app.get("/addMember", (req, res) => res.render("addMember"));
-app.get("/contact", (req, res) => res.render("contact"));
-app.get("/createAccount", (req, res) => res.render("createAccount"));
-app.get("/teamMembers", (req, res) =>
-  res.render("teamMembers", { layout: "menu" })
-);
-
-*/
 
 app.use("/utilizadores", require("./routes/utilizadores"));
 app.use("/ferias", require("./routes/ferias"));
@@ -76,53 +64,6 @@ app.use("/ferias", require("./routes/ferias"));
 const PORT = process.env.PORT || 3000;
 console.log(PORT);
 
-/*
-// Middleware to parse JSON bodies
-app.use(bodyParser.urlencoded({ extended: false }));
-
-*/
-
-/*
-// Email-sending route
-app.post("/send-email", async (req, res) => {
-  const { orgName, adress, zip, country, site, keyfname, keylname, keyEmail } = req.body;
-
-  if (!keyfname || !keylname || !keyEmail) {
-    return res.status(400).json({ error: "All fields are required." });
-  }
-
-  try {
-    // Configure the SMTP transporter
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: "queirossluiss@gmail.com", // Your email address
-        pass: "xcom jqxp bvoy ezvl", // Your email password or app-specific password
-      },
-      tls: {
-        rejectUnauthorized: false, // Accept self-signed certificates
-      },
-    });
-
-    // Email options
-    const mailOptions = {
-      from: "queirossluiss@gmail.com",
-      to: keyEmail,
-      subject: `Risk Manager Account Verification`,
-      text: `Hello ${keyfname} ${keylname},\n\nPlease click the following link to finish creating your account.\nhttp://localhost:5500/pages/password.html\n\nBest Regards.`,
-    };
-
-    // Send the email
-    await transporter.sendMail(mailOptions);
-    res.status(200).json({ message: "Email sent successfully!" });
-  } catch (error) {
-    console.error("Error sending email:", error);
-    res.status(500).json({ error: "Failed to send email." });
-  }
-});
-*/
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
